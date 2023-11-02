e7d3cd9e225507289612867bf2d08fa234618ace
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-11-02 22:33:21 +0300

    auth

--- task/auth/auth.view.ts
+++ task/auth/auth.view.ts
@@ -2,9 +2,7 @@ namespace $.$$ {
 	export class $bss_task_auth extends $.$bss_task_auth {
 		
 		rows(): readonly any[] {
-
 			const page = $mol_state_arg.value( 'page' )
-			console.log(page)
 			return [page === 'registration' ? this.Registration() : this.Login()]
 		}
 		
---
+++ task/task.view.ts
@@ -0,0 +1,18 @@
+namespace $.$$ {
+	export class $bss_task extends $.$bss_task {
+		
+		body(): readonly any[] {
+			const user = this.$.$mol_state_local.value( 'user' )
+			console.log(user)
+			return [user ? this.Deck() : this.Auth()]
+		}
+
+		logout( next?: any ) {
+			this.$.$mol_state_local.value( 'user', null )
+		}
+		tools(): readonly any[] {
+			const user = this.$.$mol_state_local.value( 'user' )
+			return [user ? this.Logout() : null]
+		}
+	}
+}
