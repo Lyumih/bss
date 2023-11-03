5268d12bd899e0afc19e9553d62f2d42f3f4b598
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-11-03 07:29:39 +0300

    update

--- task/auth/login/login.view.ts
+++ task/auth/login/login.view.ts
@@ -1,8 +1,6 @@
 namespace $.$$ {
 	export class $bss_task_auth_login extends $.$bss_task_auth_login {
 
-
-
 		email_bid(): string {
 			return this.email().endsWith( '@bss.ru' ) ? '' : this.required_message()
 		}
@@ -16,7 +14,8 @@ namespace $.$$ {
 		}
 
 		login( next?: any ) {
-			const users = this.$.$mol_state_local.value<{ email: string, password: string }[]>( 'users' )
+			const users_local = this.$.$mol_state_local.value<{ email: string, password: string }[]>( 'users' ) || []
+			const users = this.login_fetch().concat( users_local )
 			if( users && users.find( user => user.email === this.email() && user.password === this.$.$mol_base64_encode( this.password() ) ) ) {
 				this.$.$mol_state_local.value( 'user', {
 					email: this.email(),
@@ -24,6 +23,11 @@ namespace $.$$ {
 			} else {
 				this.$.$mol_fail( 'Неверный логин или пароль' )
 			}
+		}
+
+		login_fetch() {
+			const url = 'https://raw.githubusercontent.com/Lyumih/bss/master/back/api/v1/user/user.json'
+			return this.$.$mol_fetch.json( url ) as { email: string, password: string }[]
 		}
 	}
 }
